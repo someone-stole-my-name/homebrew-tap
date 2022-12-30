99c6cc663ab8dce6b26c9d8bec8cecd8c14918d9
Christian Segundo <christian.segundo@cabify.com>
2022-12-30 13:51:18 +0100

    Add ksniff

---
+++ ksniff.rb
@@ -0,0 +1,23 @@
+class Ksniff1 < Formula
+  desc "Kubectl plugin to ease sniffing on kubernetes pods using tcpdump and wireshark"
+  homepage "https://github.com/eldadru/ksniff"
+  license "Apache-2.0"
+  url "https://github.com/eldadru/ksniff/releases/download/v1.6.2/ksniff.zip"
+
+  def install
+    on_linux do
+      bin.install "kubectl-sniff"
+    end
+
+    on_macos do
+      on_arm do
+        bin.install "kubectl-sniff-darwin-arm64" => "kubectl-sniff"
+      end
+      on_intel do
+        bin.install "kubectl-sniff-darwin" => "kubectl-sniff"
+      end
+    end
+
+    bin.install "static-tcpdump"
+  end
+end
