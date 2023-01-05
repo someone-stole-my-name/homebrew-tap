1f51ec410ea23551e9c038154aa78793bc5e96e2
Christian Segundo <christian.segundo@cabify.com>
2023-01-05 03:49:32 +0100

    fix head only formulas add ksniff sha fix kubent empty install fix ksniff

--- kubent.rb
+++ kubent.rb
@@ -1,18 +1,24 @@
 class Kubent < Formula
   desc "Easily check your clusters for use of deprecated APIs"
   homepage "https://github.com/doitintl/kube-no-trouble"
+  url "https://github.com/doitintl/kube-no-trouble/archive/refs/tags/0.7.0.tar.gz"
+  sha256 "a0517831170b48648aba3fcabfd553350dc75764597a5d7c02bad6253ddeaa90"
   license "MIT"
   head "https://github.com/doitintl/kube-no-trouble.git", branch: "master"
 
   depends_on "go" => [:build]
 
   def install
-    ldflags = %W[
-      -s -w
-      -X main.version=#{Utils.git_head}
-      -X main.gitSha=#{Utils.git_head}
-    ]
+    ldflags = "-s -w"
 
-    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/kubent"
+    if build.head?
+      ldflags += " -X main.version=#{Utils.git_head}"
+      ldflags += " -X main.gitSha=#{Utils.git_head}"
+    else
+      ldflags += " -X main.version=v#{version}"
+    end
+
+    system "go", "build", "-ldflags", ldflags, "./cmd/kubent"
+    bin.install "kubent"
   end
 end
