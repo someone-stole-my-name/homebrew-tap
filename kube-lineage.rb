6777a2dd61403e35b11b3675fe900c0b1e3b9dc1
Christian Segundo <christian.segundo@cabify.com>
2023-01-11 14:41:43 +0100

    fix hash

--- kube-lineage.rb
+++ kube-lineage.rb
@@ -2,7 +2,7 @@ class KubeLineage < Formula
   desc "Display all dependencies or dependents of objects in a Kubernetes cluster"
   homepage "https://github.com/tohjustin/kube-lineage"
   url "https://github.com/tohjustin/kube-lineage/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "a0517831170b48648aba3fcabfd553350dc75764597a5d7c02bad6253ddeaa90"
+  sha256 "ae62cf88b0b8080c1abff50eb1f1f5c6a2ff51cb158eb5b17eb86310105254c3"
   license "Apache-2.0"
   head "https://github.com/tohjustin/kube-lineage.git", branch: "master"
 
